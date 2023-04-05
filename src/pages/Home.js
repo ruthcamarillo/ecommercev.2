31657d2ebe1e5a3080dbd09429541b4001d88396
rooot <119532739+ruthcamarillo@users.noreply.github.com>
2023-04-05 13:28:02 -0400

    pushing small changesg3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SlideShow from '../components/SlideShow';
 import '../styles/slide-show.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
 
@@ -10,6 +11,20 @@ function Home() {
     return (
         <div>
             <SlideShow />
+            <div className="mainhome">
+
+                <div className="homediv">
+
+                    <h2 > Find a Fragrance Today! </h2>
+                    <p> We want to help you find your signature sent today!</p>
+                    <div className="buttonhome">
+                        <button className="button2" > <a> <Link to="/products">Shop Now </Link> </a> </button>
+                        <button className="button2"> <a> <Link to="/contact">Contact Us </Link> </a> </button>
+
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     );
