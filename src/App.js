f088a90412e1408ab499fe4da07c83738b921067
rooot <119532739+ruthcamarillo@users.noreply.github.com>
2023-04-05 00:32:15 -0400

    pushing new routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 // import Footer from './components/Footer';
 // import SlideShow from './components/SlideShow';
 
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import './App.css';
+export const AppContext = React.createContext();
 function App() {
   return (
-    <div>
-      {/* <Nav />
-      < SlideShow />
-      <Footer /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ function Home() {
         <div>
             <SlideShow />
 
-
         </div>
     );
 }
